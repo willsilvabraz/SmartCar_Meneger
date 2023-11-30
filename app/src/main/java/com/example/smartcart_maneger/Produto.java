be6eabd66ff2395c0a845b657fa0038cbec7d15f
willsilvabraz <willsilvabraz@gmail.com>
2023-11-30 05:08:55 -0300

    inclusão classe produto

---
+++ app/src/main/java/com/example/smartcart_maneger/Produto.java
@@ -0,0 +1,13 @@
+package com.example.smartcart_maneger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Produto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
